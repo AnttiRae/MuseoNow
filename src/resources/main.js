924a9d5afd83a2907a2c1816c8fcc158cdd9076a
Unknown <ville.makinen2@edu.turkuamk.fi>
2018-10-29 10:59:04 +0200

    Kartta hommaa
    EI VITTU TOIMI!!!!!!

--- src/resources/main.js
+++ src/resources/main.js
@@ -1,1 +1,18 @@
-//Java scriptiii lol
+document.body.onload = addElement;
+function addElement(){
+    var div = createElement('div');
+    var newContent = document.createTextNode('Kartta missä olet?');
+    body.appendChild(div);
+
+    var currentDiv = document.getElementById('mapCont');
+    document.body.insertbefore(div, currentDiv);
+}
+var mymap = L.map('map').setView([51.505, -0.09], 13);
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(mymap);
